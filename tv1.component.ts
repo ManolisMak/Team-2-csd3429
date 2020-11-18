eea8d1854eafce6a18dea2627736c7b5a0976b13
ManolisMak <73944302+ManolisMak@users.noreply.github.com>
2020-11-18 16:43:24 +0200

    Add files via upload

---
+++ tv1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ami-fullstack-tv1',
+  templateUrl: './tv1.component.html',
+  styleUrls: ['./tv1.component.scss']
+})
+export class TV1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
